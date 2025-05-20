d8ce0941afd5610c4037c4efbeacb3003477848e
TorresAnndy <andygonzales2003@gmail.com>
2025-05-19 19:49:10 -0500

    fint area y gestion de carpetas

--- script.js
+++ script.js
@@ -15,11 +15,21 @@ function sumar(){
 function tabla(){
     const n1 = parseInt(document.getElementById("tablaNum").value);
     const lista = document.getElementById("resultadoTabla")
-    lista.innerHTML = " ";
+    lista.innerHTML = "";
     for (let i = 1; i<=10; i++){
-        const li = document.createElement("l1");
+        const li = document.createElement("li");
         li.textContent = `${n1} X ${i} = ${n1 * i}`;
         lista.appendChild(li);
-        
+
     }
+}
+
+
+function Promedio(){
+    const n1 = parseFloat(document.getElementById("num1").value);
+    const n2 = parseFloat(document.getElementById("num2").value);
+    const n3 = parseFloat(document.getElementById("num3").value);
+    const prom = (n1 + n2 + n3) / 3;
+
+    document.getElementById("resultadoProm").innerText= `Resultado = ${prom}`;
 }
